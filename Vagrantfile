78975962258ea172e7efd970a27465f1fae2ef0d
Holger Weis <holger.weis@gmail.com>
2018-01-10 14:31:06 +0100

    Add support for PHP 7

--- Vagrantfile
+++ Vagrantfile
@@ -1,18 +1,20 @@
-project_name  = "foobar"
-php_version   = "5.6"
-web_server    = "apache"
+project_slug  = "foobar"
 document_root = "public"
-ip_address    = "10.10.10.10"
+vm_ip_address = "10.10.10.10"
 
 Vagrant.configure("2") do |config|
   
   config.vm.box = "ubuntu/trusty64"
-  config.vm.hostname = project_name
-  config.vm.network :private_network, ip: ip_address
-  config.vm.synced_folder "./", "/var/www/#{project_name}", owner: "vagrant", group: "www-data", mount_options: ["dmode=775,fmode=664"]
-  config.vm.provision :shell, path: "bootstrap.sh", :args => "Vagrant #{project_name} #{php_version} #{web_server} #{document_root}"
+  config.vm.hostname = project_slug
+  config.vm.network :private_network, ip: vm_ip_address
   
-  #config.vm.provision :shell, inline: "echo '#{project_name}' > /etc/hostname && hostname '#{project_name}'"
-  #config.vm.synced_folder "./", "/var/www/#{project_name}", type: "nfs"
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+  end
+  
+  config.vm.synced_folder "./", "/var/www/#{project_slug}", owner: "vagrant", group: "www-data", mount_options: ["dmode=775,fmode=664"]
+  #config.vm.synced_folder "./", "/var/www/#{project_slug}", type: "nfs"
+  
+  config.vm.provision :shell, path: "bootstrap.sh", :args => "Vagrant #{project_slug} #{document_root}"
   
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,224 +1,132 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # ==============================================================================
 # Vagrant
 # ==============================================================================
 
+# Sanity Check 👊
 if [ "$1" != "Vagrant" ]; then
   echo "The provisioning script should not be called directly!"
   exit 1
 fi
 
-PROJECT_NAME=$2
-PHP_VERSION=$3
-WEB_SERVER=$4
-DOCUMENT_ROOT=$5
+# Passed Properties
+PROJECT_SLUG=$2
+DOCUMENT_ROOT=$3
 
 # ==============================================================================
 # System
 # ==============================================================================
 
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update
-apt-get install -y curl
-apt-get install -y git-core
-apt-get install -y vim
-
 # Locales
+apt-get install -y language-pack-de
 
-locale-gen de_DE
-locale-gen de_DE.UTF-8
+# Common Tools
+apt-get install -y git
 
 # Aliases
+echo "alias artisan=\"php artisan\"" >> /home/vagrant/.bash_aliases
 
-echo "alias artisan='php artisan'" >> /home/vagrant/.bash_aliases
+# We are no humans
+export DEBIAN_FRONTEND=noninteractive
 
 # ==============================================================================
 # PHP
 # ==============================================================================
 
-apt-get install -y python-software-properties
-
-if [ $PHP_VERSION = "5.6" ]; then
-  add-apt-repository -y ppa:ondrej/php5-5.6
-elif [ $PHP_VERSION = "5.5" ]; then
-  add-apt-repository -y ppa:ondrej/php5
-elif [ $PHP_VERSION = "5.4" ]; then
-  add-apt-repository -y ppa:ondrej/php5-oldstable
-fi
-
+# PHP 7+ PPA Repository
+add-apt-repository -y ppa:ondrej/php
 apt-get update
-apt-get install -y php-apc
-apt-get install -y php5-cli
-apt-get install -y php5-curl
-apt-get install -y php5-gd
-apt-get install -y php5-imagick
-apt-get install -y php5-mcrypt
-apt-get install -y php5-mysqlnd
-apt-get install -y php5-sqlite
-apt-get install -y php5-tidy
-apt-get install -y php5-xdebug
-apt-get install -y php5-xsl
 
-sed -i 's/;date.timezone =/date.timezone = UTC/' /etc/php5/cli/php.ini
+# PHP 7.1 common extensions
+apt-get install -y php7.1-cli
+apt-get install -y php7.1-curl
+apt-get install -y php7.1-gd
+apt-get install -y php7.1-imagick
+apt-get install -y php7.1-json
+apt-get install -y php7.1-mbstring
+apt-get install -y php7.1-mcrypt
+apt-get install -y php7.1-mysql
+apt-get install -y php7.1-tidy
+apt-get install -y php7.1-xdebug
+apt-get install -y php7.1-xml
+
+# Set the default timezone to UTC
+sed -i "s/;date.timezone =/date.timezone = UTC/" /etc/php/7.1/cli/php.ini
 
 # ==============================================================================
 # Apache
 # ==============================================================================
 
-if [ $WEB_SERVER = "apache" ]; then
-
-apt-get install -y apache2
-apt-get install -y libapache2-mod-php5
-
-echo "ServerName localhost" > /etc/apache2/httpd.conf
-
+# Default VHost
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
-    ServerName $PROJECT_NAME.dev
-    DocumentRoot /var/www/$PROJECT_NAME/$DOCUMENT_ROOT
-    <Directory "/var/www/$PROJECT_NAME/$DOCUMENT_ROOT">
+    ServerName $PROJECT_SLUG.local
+    DocumentRoot /var/www/$PROJECT_SLUG/$DOCUMENT_ROOT
+    <Directory "/var/www/$PROJECT_SLUG/$DOCUMENT_ROOT">
         AllowOverride All
     </Directory>
 </VirtualHost>
 EOF
 )
 
-rm -f /etc/apache2/sites-available/*
-rm -f /etc/apache2/sites-enabled/*
-echo "$VHOST" > /etc/apache2/sites-available/default.conf
-ln -fs /etc/apache2/sites-available/default.conf /etc/apache2/sites-enabled/default.conf
+# Apache 2 & Mod PHP
+apt-get install -y apache2
+apt-get install -y libapache2-mod-php7.1
 
-sed -i 's/;date.timezone =/date.timezone = UTC/' /etc/php5/apache2/php.ini
-sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 32M/' /etc/php5/apache2/php.ini
-
-rm -rf /var/www/html
-rm -f /var/www/index.html
-
+# Common Mods
 a2enmod rewrite
 a2enmod headers
 a2enmod expires
 
+# Clean up
+rm -f /etc/apache2/sites-available/*
+rm -f /etc/apache2/sites-enabled/*
+rm -rf /var/www/html
+
+# Create and enable a default VHost
+echo "$VHOST" > /etc/apache2/sites-available/default.conf
+ln -fs /etc/apache2/sites-available/default.conf /etc/apache2/sites-enabled/default.conf
+
+# Set the default timezone to UTC and increase upload_max_filesize
+sed -i "s/;date.timezone =/date.timezone = UTC/" /etc/php/7.1/apache2/php.ini
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 32M/" /etc/php/7.1/apache2/php.ini
+
+# Please Apache 🙄
+printf "\n\nServerName $PROJECT_SLUG" >> /etc/apache2/apache2.conf
+
+# Restart Apache
 service apache2 restart
-
-fi
-
-# ==============================================================================
-# nginx
-# ==============================================================================
-
-if [ $WEB_SERVER = "nginx" ]; then
-
-apt-get install -y nginx
-apt-get install -y php5-fpm
-
-SERVER=$(cat <<EOF
-server {
-    
-    listen 80;
-    server_name $PROJECT_NAME.dev;
-    root /var/www/$PROJECT_NAME/$DOCUMENT_ROOT;
-    
-    index index.html index.php;
-    charset utf-8;
-    
-    location / {
-        try_files \$uri \$uri/ /index.php?\$query_string;
-    }
-    
-    location = /favicon.ico { access_log off; log_not_found off; }
-    location = /robots.txt  { access_log off; log_not_found off; }
-    
-    error_page 404 /index.php;
-    
-    location ~ \.php$ {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/var/run/php5-fpm.sock;
-        fastcgi_index index.php;
-        include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        fastcgi_intercept_errors on;
-        fastcgi_buffer_size 16k;
-        fastcgi_buffers 4 16k;
-    }
-    
-    location ~ /\.ht {
-        deny all;
-    }
-    
-}
-EOF
-)
-
-echo "$SERVER" > /etc/nginx/sites-available/default
-
-service nginx restart
-service php5-fpm restart
-
-fi
 
 # ==============================================================================
 # MySQL
 # ==============================================================================
 
-apt-get install -y mysql-server-5.5
+# MySQL 5.6
+apt-get install -y mysql-server-5.6
 
-sed -i 's/bind-address\t\t= 127.0.0.1/bind-address\t\t= 0.0.0.0/' /etc/mysql/my.cnf
+# Allow remote database connections
+sed -i "s/bind-address\t\t= 127.0.0.1/bind-address\t\t= 0.0.0.0/" /etc/mysql/my.cnf
 service mysql restart
 
-mysql -u root -e "CREATE DATABASE \`$PROJECT_NAME\` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;"
+# Get the host address to grant access to
+HOST=$(ifconfig eth1 | grep "inet addr" | awk -F : '{print $2}' | awk '{print $1}' | sed "s/.[0-9]*$/.%/")
 
-HOST=$(ifconfig eth1 | grep 'inet addr' | awk -F : '{print $2}' | awk '{print $1}' | sed 's/.[0-9]*$/.%/')
+# Create a UTF8 database
+mysql -u root -e "CREATE DATABASE $PROJECT_SLUG DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;"
 
-mysql -u root -e "GRANT ALL ON \`$PROJECT_NAME\`.* TO '$PROJECT_NAME'@'localhost' IDENTIFIED BY '$PROJECT_NAME';"
-mysql -u root -e "GRANT ALL ON \`$PROJECT_NAME\`.* TO '$PROJECT_NAME'@'$HOST' IDENTIFIED BY '$PROJECT_NAME';"
+# Create a database user
+mysql -u root -e "GRANT ALL ON $PROJECT_SLUG.* TO '$PROJECT_SLUG'@'localhost' IDENTIFIED BY '$PROJECT_SLUG';"
+mysql -u root -e "GRANT ALL ON $PROJECT_SLUG.* TO '$PROJECT_SLUG'@'$HOST' IDENTIFIED BY '$PROJECT_SLUG';"
 mysql -u root -e "FLUSH PRIVILEGES;"
-
-# ==============================================================================
-# SQLite
-# ==============================================================================
-
-apt-get install -y sqlite
 
 # ==============================================================================
 # Postfix
 # ==============================================================================
 
-echo postfix postfix/mailname string $PROJECT_NAME.dev | debconf-set-selections
+# Pre-configure Postfix for mail delivery
+echo postfix postfix/mailname string $PROJECT_SLUG.local | debconf-set-selections
 echo postfix postfix/main_mailer_type string 'Internet Site' | debconf-set-selections
 
+# Postfix
 apt-get install -y postfix
-
-# ==============================================================================
-# Tools
-# ==============================================================================
-
-# Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
-# Node
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-apt-get install -y nodejs
-
-# Gulp
-#npm install --global gulp
-
-# PHPUnit
-#curl -O https://phar.phpunit.de/phpunit.phar
-#chmod 755 phpunit.phar
-#mv phpunit.phar /usr/local/bin/phpunit
-
-# PHP Coding Standards Fixer
-#curl -O http://get.sensiolabs.org/php-cs-fixer.phar
-#chmod 755 php-cs-fixer.phar
-#mv php-cs-fixer.phar /usr/local/bin/php-cs-fixer
-
-# WP-CLI
-#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-#chmod 755 wp-cli.phar
-#mv wp-cli.phar /usr/local/bin/wp
-
-# Drush
-#apt-get install -y drush
