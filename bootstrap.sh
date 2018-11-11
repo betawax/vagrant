be1ccb42d5cfa48f9bc7dfce901b0814a37f2b3b
Holger Weis <holger.weis@gmail.com>
2018-11-11 15:05:40 +0100

    Only install Composer & NPM by default

--- bootstrap.sh
+++ bootstrap.sh
@@ -162,8 +162,5 @@ mv composer.phar /usr/local/bin/composer
 curl -sL https://deb.nodesource.com/setup_9.x | bash
 apt-get install -y nodejs build-essential
 
-# Gulp
-npm install --global gulp-cli
-
 # ==============================================================================
 # ==============================================================================
